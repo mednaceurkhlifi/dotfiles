5ffdf3ea2b331215bdb2ceca595fdb75208e8ff5
naceur <naceur@localhost.localdomain>
2024-01-24 17:28:59 +0100

    first commit

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+function fish_greeting
+end
+
+
+# set -gx PATH /opt/lampp/bin $PATH 
+# set -gx PATH /opt/lampp $PATH 
